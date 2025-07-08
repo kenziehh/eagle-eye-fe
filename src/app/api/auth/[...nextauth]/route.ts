ec2c59374879374ab7ada81a535b0e950e08d23e
kenziehh <ghaisani.nurani@gmail.com>
2025-07-09 06:25:43 +0700

    fix: fix build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,9 @@
-import { authOptions } from "@/lib/auth-options";
 import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth-options";
 import { NextRequest } from "next/server";
 
-interface RouteHandlerContext {
-    params: { nextauth: string[] };
-}
-
-const handler = async (req: NextRequest, context: RouteHandlerContext) => {
-    return NextAuth(req, context, authOptions);
+const handler = async (req: NextRequest) => {
+  return NextAuth(authOptions)(req);
 };
 
 export { handler as GET, handler as POST };
