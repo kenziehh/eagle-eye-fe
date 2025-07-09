1dbe6ac75bc4d931fc9508b8c62d6a593c381ea6
kenziehh <ghaisani.nurani@gmail.com>
2025-07-09 07:04:45 +0700

    fix(auth): fix prod error and simplify NextAuth handler implementation

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth-options";
-import { NextRequest } from "next/server";
 
-const handler = async (req: NextRequest) => {
-  return NextAuth(authOptions)(req);
-};
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
